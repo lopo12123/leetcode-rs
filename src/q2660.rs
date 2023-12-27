281e2ac3e830e05f515c4f80d8b91eab9021e6c4
lopo <lopo@zju.edu.cn>
2023-12-27 22:52:29 +0800

    feat: q2660 -- p

--- src/q2660.rs
+++ src/q2660.rs
@@ -1,29 +1,35 @@
 struct Solution;
 
 impl Solution {
-    fn score(hits: Vec<i32>) -> i32 {
-        let mut score = 0;
-
-        let mut pre1 = 0;
-        let mut pre2 = 0;
-        for hit in hits {
-            let rate = if pre1 == 10 || pre2 == 10 { 2 } else { 1 };
-
-            score += hit * rate;
-
-            pre2 = pre1;
-            pre1 = hit;
-        }
-
-        score
-    }
-
     pub fn is_winner(player1: Vec<i32>, player2: Vec<i32>) -> i32 {
-        match (Solution::score(player1) - Solution::score(player2)).signum() {
-            1 => 1,
-            -1 => 2,
-            _ => 0,
+        fn score(hits: Vec<i32>) -> i32 {
+            // 每次得到 10 分即可双倍两回合
+            let mut bonus_round = 0;
+
+            // 总分
+            let mut score = 0;
+
+            for hit in hits {
+                score += hit * if bonus_round > 0 { 2 } else { 1 };
+                // score += hit + if bonus_round > 0 { hit } else { 0 };
+                bonus_round -= 1;
+
+                // 刷新但不叠加
+                if hit == 10 { bonus_round = 2 }
+            }
+
+            score
         }
+
+        let score1 = score(player1);
+        let score2 = score(player2);
+        if score1 < score2 { 2 } else if score1 > score2 { 1 } else { 0 }
+
+        // match score(player1).cmp(&score(player2)) {
+        //     std::cmp::Ordering::Less => 2,
+        //     std::cmp::Ordering::Greater => 1,
+        //     std::cmp::Ordering::Equal => 0,
+        // }
     }
 }
 
@@ -34,17 +40,19 @@ mod unit_test {
     #[test]
     fn run() {
         let test_cases = vec![
-            (vec![4, 10, 7, 9], vec![6, 5, 2, 3]),
-            (vec![3, 5, 7, 6], vec![8, 10, 10, 2]),
-            (vec![2, 3], vec![4, 1]),
-            (vec![5, 6, 1, 10], vec![5, 1, 10, 5]),
+            (vec![4, 10, 7, 9], vec![6, 5, 2, 3], 1),
+            (vec![3, 5, 7, 6], vec![8, 10, 10, 2], 2),
+            (vec![2, 3], vec![4, 1], 0),
+            (vec![5, 6, 1, 10], vec![5, 1, 10, 5], 2),
         ];
 
         for test_case in test_cases {
             let player1 = test_case.0;
             let player2 = test_case.1;
 
-            println!("result: {}", Solution::is_winner(player1, player2));
+            assert_eq!(Solution::is_winner(player1, player2), test_case.2);
         }
+
+        println!("PASS");
     }
 }
