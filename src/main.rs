89730b0195d83af12caac5ebab7eaf88e9622ea4
lopo <lopo@zju.edu.cn>
2023-12-29 22:46:53 +0800

    feat: q2706

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod q2660;
+mod q2706;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/q2706.rs
@@ -0,0 +1,28 @@
+struct Solution;
+
+impl Solution {
+    pub fn buy_choco(prices: Vec<i32>, money: i32) -> i32 {
+        let mut p1 = i32::MAX;
+        let mut p2 = i32::MAX;
+
+        for p in prices {
+            if p < p1 {
+                p2 = p1;
+                p1 = p;
+            } else if p < p2 {
+                p2 = p;
+            }
+        }
+
+        if p1 == i32::MAX || p2 == i32::MAX {
+            money
+        } else {
+            let remain = money - p1 - p2;
+            if remain < 0 {
+                money
+            } else {
+                remain
+            }
+        }
+    }
+}
