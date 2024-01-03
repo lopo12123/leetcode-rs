a8fbdf6e31527e0aa67b46c2cc1390e81c6df0f2
lopo <lopo@zju.edu.cn>
2024-01-04 00:26:39 +0800

    todo: q2487

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod q1185;
 mod q1154;
 mod q1599;
 mod q446;
+mod q2487;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/q2487.rs
@@ -0,0 +1,114 @@
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
+    }
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn remove_nodes(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        // List of tuple represents (val, index)
+        let mut stack = vec![];
+
+        // walk through
+        let mut ptr = &head.unwrap();
+        let mut ctr = 0i32;
+        loop {
+            // push
+            stack.push((ptr.val, ctr));
+
+            // check
+            if let Some(node) = &ptr.next {
+                ptr = node;
+                ctr += 1;
+            } else {
+                break;
+            }
+        };
+
+        // do filter thing
+        let mut max = i32::MIN;
+        for i in (0..stack.len()).rev() {
+            if stack[i].0 < max {
+                stack[i].1 = -1;
+            } else {
+                max = stack[i].0;
+            }
+        }
+
+        // re-chain TODO: 所有权问题
+        // fixed head
+        let mut new_head: Option<Box<ListNode>> = None;
+        // last kept node
+        let mut new_tail: Option<&Box<ListNode>> = None;
+        // pointer
+        let mut ptr = &head.unwrap();
+        // counter
+        let mut ctr = 0i32;
+        loop {
+            // kept item
+            if stack[ctr as usize].1 == ctr {
+                match new_tail {
+                    // re-assign head (init tail as well)
+                    None => {
+                        new_head = Some(ptr.to_owned());
+                        new_tail = new_head.as_ref();
+                    }
+                    // re-assign next
+                    Some(tail) => {
+                        *tail.next = Some(ptr.to_owned());
+                        // tail = ptr;
+                    }
+                }
+            }
+
+            // forward
+            ptr = ptr.as_ref().unwrap().next;
+            ctr += 1;
+
+            // stop signal
+            if ctr == stack.len() as i32 {
+                break;
+            }
+        };
+
+        new_head
+    }
+}
+
+#[cfg(test)]
+mod unit_test {
+    use super::*;
+
+    fn list_builder(v: Vec<i32>) -> Option<Box<ListNode>> {
+        let mut head = Some(Box::new(ListNode::new(v[0])));
+
+        let mut tail = head.as_mut().unwrap();
+        for i in 1..v.len() {
+            tail.next = Some(Box::new(ListNode::new(v[i])));
+            tail = tail.next.as_mut().unwrap();
+        }
+        head
+    }
+
+    #[test]
+    fn t() {
+        let l1 = list_builder(vec![5, 2, 13, 3, 8]);
+        let l2 = list_builder(vec![1, 1, 1, 1, 1]);
+
+        println!("{:#?}", Solution::remove_nodes(l1));
+        // println!("{:?}", Solution::remove_nodes(l2));
+    }
+}
