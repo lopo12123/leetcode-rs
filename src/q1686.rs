a372f54c67c1aee80aa34adbac1267a9b9aa3a1f
lopo <lopo@zju.edu.cn>
2024-02-02 22:15:58 +0800

    feat: q1686

--- src/q1686.rs
+++ src/q1686.rs
@@ -2,18 +2,17 @@ struct Solution;
 
 impl Solution {
     pub fn stone_game_vi(alice_values: Vec<i32>, bob_values: Vec<i32>) -> i32 {
-        // 取走造成的差距
         let mut diff = alice_values.iter().zip(bob_values.iter()).map(|(&a, &b)| (a + b, a, b)).collect::<Vec<(i32, i32, i32)>>();
         diff.sort_unstable_by(|a, b| {
-            a.0.cmp(&b.0)
+            b.0.cmp(&a.0)
         });
 
-        println!("{:?}", diff);
+        let mut baseline = 0;
+        for (idx, &(_, a, b)) in diff.iter().enumerate() {
+            if idx % 2 == 0 { baseline += a } else { baseline -= b }
+        }
 
-        let mut alice_score = 0;
-        let mut bob_score = 0;
-
-        -1
+        baseline.signum()
     }
 }
 
