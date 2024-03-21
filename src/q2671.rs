a4ef4efd0dc3243b1209163fbe08984e4edec4eb
lopo <lopo@zju.edu.cn>
2024-03-21 22:01:04 +0800

    feat: q2671 -- better

--- src/q2671.rs
+++ src/q2671.rs
@@ -2,7 +2,7 @@ use std::collections::{HashMap, HashSet};
 
 struct FrequencyTracker {
     n2f: HashMap<i32, i32>,
-    f2n: HashMap<i32, HashSet<i32>>,
+    f2n: HashMap<i32, i32>,
 }
 
 
@@ -16,34 +16,34 @@ impl FrequencyTracker {
     }
 
     fn add(&mut self, number: i32) {
-        // 频率加一, 如果没有则默认为1
-        let f_now = self.n2f.entry(number).and_modify(|f| *f += 1).or_insert(1);
-        // 在新频率中加入该数字
-        self.f2n.entry(*f_now).or_insert(HashSet::new()).insert(number);
-        // 如果频率不为1, 说明该数字之前已经存在, 需要删除旧的频率中的该数字
-        if *f_now > 1 {
-            self.f2n.get_mut(&(*f_now - 1)).map(|set| set.remove(&number));
+        // 修改该数字的频率
+        let f_new = self.n2f.entry(number).and_modify(|v| *v += 1).or_insert(1);
+        if *f_new > 1 {
+            // 旧频率对应数字-1
+            self.f2n.get_mut(&(*f_new - 1)).map(|v| *v -= 1);
         }
+        // 新频率对应数字+1
+        self.f2n.entry(*f_new).and_modify(|v| *v += 1).or_insert(1);
     }
 
     fn delete_one(&mut self, number: i32) {
-        // 如果有该数字则执行操作
-        if let Some(spec) = self.n2f.get_mut(&number) {
-            // 如果频率为0, 说明该数字已经被删除, 直接返回
-            if *spec == 0 {
-                return;
-            }
-
-            // 原有频率中删去该数字
-            self.f2n.get_mut(spec).map(|set| set.remove(&number));
-            // 频率减一
-            *spec -= 1;
-            // 新频率中加入该数字
-            self.f2n.get_mut(spec).map(|set| set.insert(number));
+        // 存在且频率大于等于1才执行操作
+        if self.n2f.get(&number).is_some_and(|v| *v >= 1) {
+            // 修改该数字的频率
+            let freq = self.n2f.get_mut(&number).unwrap();
+            // 旧频率对应数字-1
+            self.f2n.get_mut(freq).map(|v| *v -= 1);
+            *freq -= 1;
+            // 新频率对应数字+1
+            self.f2n.get_mut(freq).map(|v| *v += 1);
         }
     }
 
     fn has_frequency(&self, frequency: i32) -> bool {
-        self.f2n.get(&frequency).map_or(false, |set| !set.is_empty())
+        self.f2n.get(&frequency).map_or(false, |v| *v > 0)
+    }
+
+    pub fn log(&self) {
+        println!("n2f: {:?}\nf2n: {:?}\n======", self.n2f, self.f2n);
     }
 }
